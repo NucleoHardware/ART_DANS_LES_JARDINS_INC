ec42a677554942908f0ad3dde84a8a16124db134
Gabriel <enriquezg411@gmail.com>
2025-01-26 01:03:43 -0600

    integration of carrousel of image in the section winter

--- winter.js
+++ winter.js
@@ -1,3 +1,4 @@
+//nieve
 const TOTAL_SNOWFLAKES = 30;
 
 function createSnowflake() {
@@ -25,4 +26,41 @@ function createSnowflake() {
 
 for (let i = 0; i < TOTAL_SNOWFLAKES; i++) {
     setTimeout(createSnowflake, i * 200);
+}
+
+
+//carrousel
+function App() {}
+
+    window.onload = function(event) {
+        var app = new App();
+        window.app = app;
+    }
+
+App.prototype.processingButton = function(event){
+    const btn = event.currentTarget;
+    const carruselList = event.currentTarget.parentNode;
+    const track = event.currentTarget.parentNode.querySelector('#track');
+    const carrusel = track.querySelectorAll('.carrusel');
+
+const carruselWidth = carrusel[0].offsetWidth;
+
+const trackWidth = track.offsetWidth;
+const listWidht = carruselList.offsetWidth;
+
+track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1);
+btn.dataset.button == "button-prev" ? prevAction(leftPosition,carruselWidth, track) : nextAction(leftPosition, trackWidth, listWidht,carruselWidth, track);
+
+}
+
+ let prevAction = (leftPosition,carruselWidth,track)=>{
+    if(leftPosition > 0){
+        track.style.left = `${-1 * (leftPosition - carruselWidth)}px`;
+    }
+}
+
+let nextAction = (leftPosition,trackWidth,listWidht,carruselWidth,track)=>{
+    if(leftPosition < (trackWidth - listWidht)){
+        track.style.left = `${-1 * (leftPosition + carruselWidth)}px`;
+    }
 }
