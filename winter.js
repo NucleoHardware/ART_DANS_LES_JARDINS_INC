4dbac47b70a3290b1d6867ea8ab2f029c14d326d
Gabriel <enriquezg411@gmail.com>
2025-01-26 18:06:16 -0600

    add animation of winter

--- winter.js
+++ winter.js
@@ -1,5 +1,26 @@
-// ...existing code...
-
+//nieve
+const TOTAL_SNOWFLAKES = 30;
+function createSnowflake() {
+    const snowflake = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    snowflake.setAttribute("class", "snowflake");
+    snowflake.setAttribute("width", "20");
+    snowflake.setAttribute("height", "20");
+    snowflake.setAttribute("viewBox", "0 0 24 24");
+    snowflake.style.left = Math.random() * 100 + "vw";
+    snowflake.style.animationDuration = Math.random() * 5 + 5 + "s"; 
+    snowflake.style.animationDelay = Math.random() * 5 + "s"; 
+    snowflake.innerHTML = `
+        <path fill="white" d="M12 2L13 8H17L14 10L15 16L12 14L9 16L10 10L7 8H11L12 2Z"/>
+    `;
+    document.body.appendChild(snowflake);
+    snowflake.addEventListener("animationend", () => {
+        snowflake.remove();
+        createSnowflake(); 
+    });
+}
+for (let i = 0; i < TOTAL_SNOWFLAKES; i++) {
+    setTimeout(createSnowflake, i * 200);
+}
 //carrousel
 function App() {}
 
